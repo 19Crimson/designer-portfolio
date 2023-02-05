e746a8932f9ef87f084a933a1a472fa470e83e58
Sunny Seraje <snowdulum@gmail.com>
2023-02-05 21:52:17 +0300

    cleared extra demo projects

--- src/assets/configs/projects.ts
+++ src/assets/configs/projects.ts
@@ -13,36 +13,6 @@ const content = [
     title: 'Geometry',
     titleColor: 'white',
   },
-  {
-    project: 'Demo',
-    cardBg: 'bg-2.png',
-    cardFg: 'fg-2.png',
-    title: 'Geometry',
-    titleColor: 'white',
-  },
-  {
-    project: 'Demo',
-    cardBg: 'bg-3.png',
-    cardFg: 'fg-3.png',
-    title: 'Trees',
-    titleColor: 'white',
-  },
-  {
-    project: 'Demo',
-    cardBg: 'bg-4.png',
-    cardFg: 'fg-4.png',
-    title: 'Custom pattern',
-    titleColor: 'white',
-    gradientColor: '77, 45, 45',
-  },
-  {
-    project: 'Demo',
-    cardBg: 'bg-5.png',
-    cardFg: 'fg-5.png',
-    title: 'Build your own design',
-    titleColor: 'white',
-    gradientColor: '0, 255, 204',
-  },
 ];
 
 export default content;
