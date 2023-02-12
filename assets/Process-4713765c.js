28a627fbb9c1bb0d6b336a5e450125138db771aa
Sunny Seraje <snowdulum@gmail.com>
2023-02-12 04:36:42 +0300

    Updates

--- assets/Process-4713765c.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r,o as c,e as n}from"./index-96c5fd8f.js";const a={};function s(t,_){const e=r("ImageWrap");return c(),n(e,{image:"6.png"})}const m=o(a,[["render",s]]);export{m as default};
---
+++ assets/Process-749d857f.js
@@ -0,0 +1,1 @@
+import{_ as o,r,o as c,e as n}from"./index-14ed21cb.js";const s={};function a(t,_){const e=r("ImageWrap");return c(),n(e,{src:"6.png"})}const m=o(s,[["render",a]]);export{m as default};
